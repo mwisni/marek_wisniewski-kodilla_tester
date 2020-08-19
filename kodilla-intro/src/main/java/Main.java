82e2867c22d470bcd8eca01269c5b604a4aea101
Marek Winiewski <marekwisniewski92@gmail.com>
2020-08-19 14:26:00 +0200

    various_variables

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        String freeText = "Hello from FirstClass!";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+
+        System.out.println(freeText);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String fullName = "Marek Wiśniewski";
+        String hobby = "Sport";
+        int age = 28;
+        double weight = 87.5;
+        boolean likeTesting = true;
+        char firstLetterName = 'M';
+
+        System.out.println("Imię i nazwisko: " + fullName);
+        System.out.println("Interesuje mnie: " + hobby);
+        System.out.println("Mój wiek to: " + age);
+        System.out.println("Waga: " + weight);
+        System.out.println("Lubię testować: " + likeTesting);
+        System.out.println("Pierwsza litera imienia to: " + firstLetterName);
+
+
+    }
+}
