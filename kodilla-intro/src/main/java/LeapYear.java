b81468e65f9644d4e10bde6b02e853bb0ede1f4f
Marek Winiewski <marekwisniewski92@gmail.com>
2020-08-20 20:21:23 +0200

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+    public static void main(String[] args) {
+
+        int year = 1996;
+
+
+        if (year % 4 == 0) {
+            System.out.println("Rok jest przestępny");
+        }
+            else{
+                System.out.println("Rok nie jest przestępny");
+            }
+
+    }
+}
+
+
