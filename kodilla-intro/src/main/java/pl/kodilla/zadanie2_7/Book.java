71e01b76574d73dac33521b64a53a6a2c9bb7c18
Marek Winiewski <marekwisniewski92@gmail.com>
2020-09-27 20:33:15 +0200

    Zadanie2_7

--- kodilla-intro/src/main/java/pl/kodilla/zadanie2_7/Book.java
+++ kodilla-intro/src/main/java/pl/kodilla/zadanie2_7/Book.java
@@ -3,9 +3,24 @@ package pl.kodilla.zadanie2_7;
 public class Book {
 
 
-    public static void main(String[] args) {
-
-        BookAtribut bookAtribut = BookAtribut.of("Katty Sierra", "JAVA Rusz głową");
-        System.out.println(bookAtribut);
+    private  String author;
+    private  String title;
+// Konstruktor Book, z argumentami Autor i Tytuł
+    private Book(String author, String title){
+        this.author = author;
+        this.title = title;
     }
+
+// Metoda Zwracająca argumenty Książki (autor i tytuł)
+    public static Book of(String author, String title){
+        Book bookAtribut = new Book(author, title);
+
+        return bookAtribut;
+    }
+// Metoda toString, konwertuje wartości do Stringa
+    public String toString (){
+        return "Autor: " + this.author + " Tytuł: " + this.title;
+    }
+
+
 }
---
+++ kodilla-intro/src/main/java/pl/kodilla/zadanie2_7/Main.java
@@ -0,0 +1,11 @@
+package pl.kodilla.zadanie2_7;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Book bookAtribut = Book.of("Katty Sierra", "JAVA Rusz głową");
+        System.out.println(bookAtribut);
+    }
+}
