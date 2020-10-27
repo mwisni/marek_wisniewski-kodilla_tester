522a7b0a9714d62822a2b45b66b723f0cc7bb3d8
Marek Winiewski <marekwisniewski92@gmail.com>
2020-10-27 11:26:59 +0100

    update CarRace

--- kodilla-collections/src/main/java/homework/CarRace.java
+++ kodilla-collections/src/main/java/homework/CarRace.java
@@ -4,13 +4,13 @@ public class CarRace {
 
     public static void main(String[] args) {
 
-        Ford ford = new Ford();
+        Ford ford = new Ford(12, 15);
         doRace(ford);
         System.out.println("Ford speed is: " + ford.getSpeed());
-        Opel opel = new Opel();
+        Opel opel = new Opel(25,15);
         doRace(opel);
         System.out.println("Opel speed is: " + opel.getSpeed());
-        Skoda skoda = new Skoda();
+        Skoda skoda = new Skoda(25, 100);
         doRace(skoda);
         System.out.println("Skoda speed is: " + skoda.getSpeed());
     }
