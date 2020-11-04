91bcf7b7f1cd4768e5627d55df4d4abee9762c57
Marek Winiewski <marekwisniewski92@gmail.com>
2020-11-04 20:17:36 +0100

    5.3

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
@@ -0,0 +1,28 @@
+package com.kodilla.collections.adv.immutable;
+
+public class Book {
+
+    protected String author;
+    protected String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
