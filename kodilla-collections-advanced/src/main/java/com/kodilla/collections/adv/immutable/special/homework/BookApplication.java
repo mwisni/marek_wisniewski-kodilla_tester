06f14ce168e97c747dd0ca23849e1c86619d5a7b
Marek Winiewski <marekwisniewski92@gmail.com>
2020-11-15 08:20:22 +0100

    Zad 5_4

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -16,9 +16,8 @@ public class BookApplication {
         Book book2 = bookManager.createBook("Lśnienie", "Stephen King");
         Book book3 = bookManager.createBook("Dziewczyny które zabiły Cloe", "Alex Marwood");
 
-        // porównanie...
         System.out.println(book1.equals(book2));
-        System.out.println(book1 == book3);
+        System.out.println(book1.equals(book3));
         System.out.println(bookManager.bookSet.size());
 
         for (Book bookSet : bookManager.bookSet) {
