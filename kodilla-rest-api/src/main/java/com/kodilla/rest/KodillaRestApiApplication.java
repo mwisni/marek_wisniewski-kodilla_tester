17c5ece67b3325c3467861857e027c98045889e3
Marek Winiewski <marekwisniewski92@gmail.com>
2020-11-30 23:11:50 +0100

    10.3

--- kodilla-rest-api/src/main/java/com/kodilla/rest/KodillaRestApiApplication.java
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/KodillaRestApiApplication.java
@@ -1,7 +1,7 @@
 package com.kodilla.rest;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodillaRestApiApplication {
