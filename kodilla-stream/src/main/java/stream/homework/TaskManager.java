460ddcebffc2c7aae3ff4b8f5003591ba36b03d6
Marek Winiewski <marekwisniewski92@gmail.com>
2020-12-04 17:45:30 +0100

    6.1

--- kodilla-stream/src/main/java/stream/homework/TaskManager.java
+++ kodilla-stream/src/main/java/stream/homework/TaskManager.java
@@ -1,6 +1,7 @@
 package stream.homework;
 
 import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 public class TaskManager {
             public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class TaskManager {
                         .stream()
                         .filter(task -> task.getDeadline().isAfter(LocalDate.now()))
                         .map(Task -> Task.getDeadline())
+                        .collect(Collectors.toList())
                         .forEach(System.out::println);
 
 
